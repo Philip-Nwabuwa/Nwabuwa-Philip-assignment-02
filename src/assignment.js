a1a4448adf6678bfe0ecd01e0b0eccadd34ac092
Philip John <95682231+Philipjoe21@users.noreply.github.com>
2022-07-03 20:51:55 +0000

    solved the assignment.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+ return arrayOfNumbers.reduce((prev, current) => prev + current);
+
+  return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +30,12 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  evenNumbers = arrayOfNumbers.filter((number) => 
+    (number % 2) === 0)
+  
+    return evenNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -56,4 +61,3 @@ function celsiusToFahrenheit(arrayOfNumbers) {
 // DO NOT EDIT THIS BLOCK
 module.exports = assignment;
 // ========================
-
